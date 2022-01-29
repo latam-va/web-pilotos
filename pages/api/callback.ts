227884f15bb688b0de26c3fe4062f791fbbeea58
Francisco Gonzalez <frangc0206@gmail.com>
2022-01-29 01:30:53 -0300

    Updated callback URL

--- pages/api/callback.ts
+++ pages/api/callback.ts
@@ -2,7 +2,7 @@ import auth0 from "../../utils/auth0";
 
 export default async function callback(req: any, res: any) {
   try {
-    await auth0.handleCallback(req, res, { redirectUri: "http://localhost:3000" });
+    await auth0.handleCallback(req, res, { redirectUri: "https://pilotos.latamva.com.ar" });
   } catch (error: any) {
     console.error(error);
     res.status(error.status || 400).end(error.message);
